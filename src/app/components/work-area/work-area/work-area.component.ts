68265817a1c5fb09d9da286b492ca3f525cf7177
Liiioooo <preissegger.leo@a1.net>
2019-08-30 23:39:05 +0200

    add basic positioning for boxes add pixi.js rendering canvas

--- src/app/components/work-area/work-area/work-area.component.ts
+++ src/app/components/work-area/work-area/work-area.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild} from '@angular/core';
+import * as PIXI from 'pixi.js';
+import {fromEvent, Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
 	selector: 'app-work-area',
 	templateUrl: './work-area.component.html',
 	styleUrls: ['./work-area.component.scss']
 })
-export class WorkAreaComponent implements OnInit {
+export class WorkAreaComponent implements OnInit, OnDestroy {
 
-	constructor() { }
+	private destroy: Subject<any> = new Subject<any>();
+
+	private pixiRenderer: PIXI.Renderer;
+	private mainStage: PIXI.Container;
+
+	private pixiTicker: PIXI.Ticker;
+
+	@ViewChild('pixiCanvasContainer', {static: true})
+	private pixiCanvasContainer: ElementRef<HTMLDivElement>;
+
+	constructor(private renderer2: Renderer2) { }
 
 	ngOnInit() {
+		this.initPixi();
+		this.initPixiTicker();
+	}
+
+	private initPixi() {
+		this.pixiRenderer = new PIXI.Renderer({
+			height: this.pixiCanvasContainer.nativeElement.offsetHeight,
+			width: this.pixiCanvasContainer.nativeElement.offsetWidth,
+			antialias: true,
+			resolution: window.devicePixelRatio || 1
+		});
+		this.renderer2.appendChild(this.pixiCanvasContainer.nativeElement, this.pixiRenderer.view);
+		this.mainStage = new PIXI.Container();
+
+		fromEvent(window, 'resize').pipe(
+			takeUntil(this.destroy)
+		).subscribe(_ => {
+			this.pixiRenderer.resize(this.pixiCanvasContainer.nativeElement.offsetWidth, this.pixiCanvasContainer.nativeElement.offsetHeight);
+		});
+	}
+
+	private initPixiTicker() {
+		this.pixiTicker = new PIXI.Ticker();
+		this.pixiTicker.add(() => {
+			this.pixiRenderer.render(this.mainStage);
+		});
+		this.pixiTicker.start();
+
+		// TODO: subscribe to active tab and pause/start the ticker (rendering)
+	}
+
+	ngOnDestroy(): void {
+		this.destroy.next();
+		this.destroy.unsubscribe();
 	}
 
 }
