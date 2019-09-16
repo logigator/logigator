5d21a5a78788d14a4fd7416bfeefbc81fd99beef
Andreas <andreas.sch4@gmail.com>
2019-09-16 21:02:17 +0200

    less artifacts if devicePixelRatio is not a full number better behavior when dragging

--- src/app/components/work-area/work-area.component.ts
+++ src/app/components/work-area/work-area.component.ts
@@ -60,7 +60,7 @@ export class WorkAreaComponent implements OnInit, OnDestroy {
 			antialias: false,
 			powerPreference: 'high-performance',
 			backgroundColor: this.theming.getEditorColor('background'),
-			resolution: window.devicePixelRatio || 1,
+			resolution: Math.ceil(window.devicePixelRatio || 1),
 			autoDensity: true
 		});
 		this.renderer2.appendChild(this._pixiCanvasContainer.nativeElement, this._pixiRenderer.view);
