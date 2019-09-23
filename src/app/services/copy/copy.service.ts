9e6fba84045cb63327c53952b30533ea2a5012cb
Leo Preissegger <preissegger.leo@gmail.com>
2019-09-23 22:56:59 +0200

    fix copy service

--- src/app/services/copy/copy.service.ts
+++ src/app/services/copy/copy.service.ts
@@ -13,7 +13,7 @@ export class CopyService {
 
 	private _copiedElements: Element[] = [];
 
-	private _copiedConnections: PIXI.Point[];
+	private _copiedConPoints: PIXI.Point[];
 
 	private _sortedElements: Element[];
 
@@ -21,12 +21,16 @@ export class CopyService {
 		CopyService.staticInstance = this;
 	}
 
+	public copySelection() {
+		const elementIds = this.selection.selectedIds();
+		this.copyIds(elementIds);
+		this.copyConPoints(this.selection.selectedConnections());
+	}
+
 	public copyIds(ids: number[]): Element[] {
 		return this.copyElements(ProjectsService.staticInstance.currProject.currState.getElementsById(ids));
 	}
-
 	public copyElements(elements: Element[]): Element[] {
-		delete this._sortedElements;
 		this._copiedElements = new Array(elements.length);
 		for (let i = 0; i < elements.length; i++) {
 			this._copiedElements[i] = Elements.clone(elements[i]);
@@ -34,29 +38,24 @@ export class CopyService {
 		return this._copiedElements;
 	}
 
-	public copySelection() {
-		const elementIds = this.selection.selectedIds();
-		this.copyIds(elementIds);
-		this.copyConnectionPoints(this.selection.selectedConnections());
-	}
-
-	private copyConnectionPoints(points: PIXI.Point[]) {
-		this._copiedConnections = new Array(points.length);
+	public copyConPoints(points: PIXI.Point[]): PIXI.Point[] {
+		this._copiedConPoints = new Array(points.length);
 		for (let i = 0; i < points.length; i++) {
-			this._copiedConnections[i] = points[i].clone();
+			this._copiedConPoints[i] = points[i].clone();
 		}
+		return this._copiedConPoints;
 	}
 
-	public get copiedElements(): Element[] {
-		return this._copiedElements;
+	get copiedElements(): Element[] {
+		return this.copyElements(this._copiedElements);
 	}
 
-	public get copiedConnections(): PIXI.Point[] {
-		return this._copiedConnections;
+	get copiedConPoints(): PIXI.Point[] {
+		return this.copyConPoints(this._copiedConPoints);
 	}
 
-	private sortSelection() {
-		this._sortedElements = this._copiedElements.sort((a, b) => {
+	private sortSelection(): Element[] {
+		return this._copiedElements.sort((a, b) => {
 			if (a.pos.x < b.pos.x || a.pos.y < b.pos.y) return -1;
 			if (a.pos.x > b.pos.x || a.pos.y > b.pos.y) return 1;
 			return 0;
@@ -64,7 +63,7 @@ export class CopyService {
 	}
 
 	public getCopiedElementsBoundingBox(): {start: PIXI.Point, end: PIXI.Point} {
-		if (!this._sortedElements) this.sortSelection();
+		const sorted = this.sortSelection();
 		return {
 			start: this._sortedElements[0].pos.clone(),
 			end: this._sortedElements[this._sortedElements.length - 1].endPos.clone()
