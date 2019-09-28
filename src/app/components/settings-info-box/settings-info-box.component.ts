65a7e234bc732a77493d80e47d557efd55c63516
Leo Preissegger <preissegger.leo@gmail.com>
2019-09-28 16:46:52 +0200

    settings-info-box-form is in sync if it is not possible to change settings

--- src/app/components/settings-info-box/settings-info-box.component.ts
+++ src/app/components/settings-info-box/settings-info-box.component.ts
@@ -74,10 +74,14 @@ export class SettingsInfoBoxComponent implements OnChanges, OnDestroy {
 		});
 		this.formSubscription = this.propertiesFrom.valueChanges.subscribe((data: any) => {
 			if (data.rotation !== element.rotation) {
-				this.projects.currProject.rotateComponent(this.selectedCompId, Number(data.rotation));
+				if (!this.projects.currProject.rotateComponent(this.selectedCompId, Number(data.rotation))) {
+					this.propertiesFrom.controls.rotation.setValue(element.rotation);
+				}
 			}
 			if (data.numInputs !== element.numInputs && data.numInputs <= this.elemType.maxInputs && data.numInputs >= this.elemType.minInputs) {
-				this.projects.currProject.setNumInputs(this.selectedCompId, data.numInputs);
+				if (!this.projects.currProject.setNumInputs(this.selectedCompId, data.numInputs)) {
+					this.propertiesFrom.controls.numInputs.setValue(element.numInputs);
+				}
 			}
 		});
 	}
