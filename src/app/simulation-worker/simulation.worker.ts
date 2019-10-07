03df9aa1e5e9ccd49b873c929762699934d04596
Andreas <andreas.sch4@gmail.com>
2019-10-07 21:53:41 +0200

    some wasm testing

--- src/app/simulation-worker/simulation.worker.ts
+++ src/app/simulation-worker/simulation.worker.ts
@@ -1,5 +1,19 @@
 /// <reference lib="webworker" />
 
+let importObject = {
+	imports: {
+		imported_func(arg) {
+			console.log(arg);
+		}
+	}
+};
+
+fetch('assets/simulation.wasm').then(response =>
+	response.arrayBuffer()
+).then(bytes =>
+	WebAssembly.instantiate(bytes)
+);
+
 addEventListener('message', ({ data }) => {
 	const response = `worker response to ${data}`;
 	console.log(data);
