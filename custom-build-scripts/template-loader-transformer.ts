ac195c1ab6de828d8fa78ac48456d9561a2c9bea
Leo Preissegger <preissegger.leo@gmail.com>
2019-10-09 17:25:03 +0200

    edit transform

--- custom-build-scripts/template-loader-transformer.ts
+++ custom-build-scripts/template-loader-transformer.ts
@@ -3,8 +3,10 @@ import * as ts from 'typescript';
 export const templateLoaderTransformer = <T extends ts.Node>(context: ts.TransformationContext) => {
 	return (rootNode: ts.SourceFile) => {
 		function visit(node: ts.Node): ts.Node {
-			if (ts.isDecorator(node)) {
-				console.log(node.expression.getFullText())
+			if (ts.isDecorator(node) && (node.expression as ts.CallExpression).expression.getText() === 'Component') {
+				const arg = (node.expression as ts.CallExpression).arguments[0] as ts.ObjectLiteralExpression;
+				const templateProp = arg.properties.find(p => p.name.getText() === 'templateUrl') as ts.PropertyAssignment;
+				ts.createCall(ts.createIdentifier('require'), [], []);
 			}
 			return ts.visitEachChild(node, visit, context);
 		}
