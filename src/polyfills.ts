357e0deadaaf7457860f6f7783d32a93d22b2f04
Leo Preissegger <preissegger.leo@gmail.com>
2019-11-03 17:01:28 +0100

    set scale mode nearest

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -58,7 +58,8 @@
 (window as any).__Zone_disable_requestAnimationFrame = true;
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-// import * as PIXI from 'pixi.js';
+import * as PIXI from 'pixi.js';
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 // PIXI.settings.ROUND_PIXELS = true;
 // PIXI.Ticker.shared.autoStart = false;
 // PIXI.Ticker.shared.destroy();
