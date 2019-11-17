962a4727dbacb9fc1142cd43fa9ac5961249210f
Leo Preissegger <preissegger.leo@gmail.com>
2019-11-17 15:16:46 +0100

    show work mode in status-bar

--- src/app/components/status-bar/status-bar.component.ts
+++ src/app/components/status-bar/status-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {SelectionService} from '../../services/selection/selection.service';
 import {ProjectsService} from '../../services/projects/projects.service';
+import {Observable} from 'rxjs';
+import {WorkModeService} from '../../services/work-mode/work-mode.service';
 
 @Component({
 	selector: 'app-status-bar',
@@ -11,7 +13,8 @@ export class StatusBarComponent {
 
 	constructor(
 		private selectionService: SelectionService,
-		private projectsService: ProjectsService
+		private projectsService: ProjectsService,
+		private workMode: WorkModeService
 	) {}
 
 	public get selected(): number {
@@ -21,6 +24,10 @@ export class StatusBarComponent {
 	public get isSaved(): boolean {
 		if (!this.projectsService.currProject) return true;
 		return !this.projectsService.currProject.saveDirty;
+	}
+
+	public get workModeDescription$(): Observable<string> {
+		return this.workMode.workModeDescription$;
 	}
 }
 
