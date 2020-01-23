6f5ddc22c592a605d07986714f3f3df56cd83d7e
Leo Preissegger <preissegger.leo@gmail.com>
2020-01-24 00:04:10 +0100

    fix not using correct workerCommunicationService

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,19 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-	production: false,
-	chunkSize: 16,
-	gridPixelWidth: 16,
-	apiPrefix: 'http://api.logigator-local-dev.com',
-	domain: 'http://editor.logigator-local-dev.com'
+export declare const environment: {
+	production: boolean,
+	chunkSize: number,
+	gridPixelWidth: number,
+	apiPrefix: string,
+	domain: string
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+export declare const ELECTRON: boolean;
