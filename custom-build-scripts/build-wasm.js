fbb00e6cfd3e434d5db6b98f4258b5d789e3724e
Leo Preissegger <preissegger.leo@gmail.com>
2020-01-24 00:45:46 +0100

    fix build-wasm.js

--- custom-build-scripts/build-wasm.js
+++ custom-build-scripts/build-wasm.js
@@ -6,5 +6,5 @@ const dest = path.join(__dirname, '..', 'src', 'assets', 'wasm');
 
 if (fs.existsSync(path.join(source, 'logigator-simulation.js')) && fs.existsSync(path.join(source, 'logigator-simulation.wasm'))) {
 	fs.copyFileSync(path.join(source, 'logigator-simulation.wasm'), path.join(dest, 'logigator-simulation.wasm'));
-	fs.writeFileSync(path.join(dest, 'logigator-simulation.js'), fs.readFileSync(path.join(source, 'logigator-simulation.js')).toString().replace(/var wasmBinaryFile = '(.*?)';/, 'var wasmBinaryFile = \'assets/wasm/$1\';'));
+	fs.writeFileSync(path.join(dest, 'logigator-simulation.js'), fs.readFileSync(path.join(source, 'logigator-simulation.js')).toString().replace(/var wasmBinaryFile ?= ?["'](.*?)["'];/, 'var wasmBinaryFile = \'assets/wasm/$1\';'));
 }
