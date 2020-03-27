a96f20089bc7f83ab1a10522e36e184383d49304
Huldula <38780229+Huldula@users.noreply.github.com>
2020-03-27 18:46:45 +0100

    maybe it works

--- src/app/services/simulation/state-compiler/state-compiler.service.ts
+++ src/app/services/simulation/state-compiler/state-compiler.service.ts
@@ -253,15 +253,26 @@ export class StateCompilerService {
 					linkId, unitElems, compiledComp) + 1;
 			}
 			if (this.elementProvider.getElementById(element.typeId).ignoreOutputs) {
-				for (let i = 0; i < element.numOutputs; i++) {
-					unitElems.elementToUnit.get(element).outputs[i] = linkId++;
-				}
+				linkId = this.setIgnoreOutputsLinks(unitElems, element, linkId);
 			}
 			if (this.elementProvider.isPlugElement(element.typeId)) {
 				compiledComp.plugsByIndex.set(element.plugIndex, unitIndex);
 			}
 			unitIndex++;
 		}
+	}
+
+	private setIgnoreOutputsLinks(unitElems: UnitElementBidir, element: Element, linkId: number,): number {
+		for (let i = 0; i < element.numOutputs; i++) {
+			const wireIndex = element.numInputs + i;
+			unitElems.elementToUnit.get(element).outputs[i] = linkId++;
+			MapHelper.pushInMapArray(
+				this._wireEndsOnLinksCache.get(this._currTypeId),
+				linkId,
+				{component: element, wireIndex }
+				);
+		}
+		return linkId;
 	}
 
 	private setLinks(
