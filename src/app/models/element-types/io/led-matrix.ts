f00f2bbc38b4c7a83f35f31a8ad793fad02d060f
Leo Preissegger <preissegger.leo@gmail.com>
2020-03-27 12:35:00 +0100

    led-matrix labals

--- src/app/models/element-types/io/led-matrix.ts
+++ src/app/models/element-types/io/led-matrix.ts
@@ -19,7 +19,7 @@ export const ledMatrix: ElementType = {
 	isRotatable: true,
 	rotation: 0,
 
-	numOutputs: 0,
+	numOutputs: 16,
 
 	numInputs: 7,
 
@@ -45,28 +45,61 @@ export const ledMatrix: ElementType = {
 	onOptionsChanged(element?) {
 		const size = element ? element.options[0] : this.options[0];
 
-		let inputs: number;
+		let inputs, outputs: number;
 		switch (size) {
 			case 4:
 				inputs = 7;
+				outputs = 16;
 				break;
 			case 8:
 				inputs = 12;
+				outputs = 64;
 				break;
 			case 16:
 				inputs = 14;
+				outputs = 256;
 				break;
 		}
 
 		if (element) {
 			element.numInputs = inputs;
+			element.numOutputs = outputs;
 			return;
 		}
 		this.numInputs = inputs;
+		this.numOutputs = outputs;
 	},
 
 	calcLabels(element?) {
-		return new Array<string>(element ? element.numInputs : this.numInputs).fill('ABC');
+		const inputs = element ? element.numInputs : this.numInputs;
+		const size = element ? element.options[0] : this.options[0];
+
+		const labels = new Array<string>(inputs);
+
+		let addressAmount: number;
+		switch (size) {
+			case 4:
+				addressAmount = 2;
+				break;
+			case 8:
+				addressAmount = 3
+				break;
+			case 16:
+				addressAmount = 5;
+				break;
+		}
+		for (let a = 0; a < addressAmount; a++) {
+			labels[a] = 'A' + a;
+		}
+
+		const dataAmount = size < 8 ? 4 : 8;
+		for (let d = 0; d < dataAmount; d++) {
+			labels[d + addressAmount] = 'D' + d;
+		}
+
+		labels[inputs - 1] = 'CLK';
+
+		return labels;
 	}
 
 };
