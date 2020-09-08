cfb7610857fad50fbb0e0401abfce591effad134
Leo Preissegger <leo@webundco.com>
2020-09-08 22:19:55 +0200

    change build-wasm script to ts

---
+++ custom-build-scripts/build-wasm.ts
@@ -0,0 +1,23 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const source = path.join(__dirname, '..', '..', 'node_modules', '@logigator', 'logigator-simulation', 'webAssembly', 'dist');
+const dest = path.join(__dirname, '..', '..', 'src', 'assets', 'wasm');
+
+const timestamp = Date.now();
+
+if (fs.existsSync(path.join(source, 'logigator-simulation.js')) && fs.existsSync(path.join(source, 'logigator-simulation.wasm'))) {
+	fs.readdirSync(dest)
+		.filter(file => file.endsWith('.wasm'))
+		.forEach(file => fs.unlinkSync(path.join(dest, file)));
+
+	fs.copyFileSync(path.join(source, 'logigator-simulation.wasm'), path.join(dest, `logigator-simulation.${timestamp}.wasm`));
+
+	const simulationJs = fs.readFileSync(path.join(source, 'logigator-simulation.js'))
+		.toString()
+		.replace(/var wasmBinaryFile ?= ?["'](.*?)["'];/, `var wasmBinaryFile = \'assets/wasm/logigator-simulation.${timestamp}.wasm\';`);
+
+	fs.writeFileSync(path.join(dest, 'logigator-simulation.js'), simulationJs);
+} else {
+	process.exit(1);
+}
--- custom-build-scripts/markdown-postprocess-loader.ts
+++ custom-build-scripts/markdown-postprocess-loader.ts
@@ -1,4 +1,4 @@
-export default function (source: string): string {
+export default function(source: string): string {
 	source = source.replace(/\.\.\/\.\.\/assets\//g, 'assets/');
 	return 'module.exports = ' + JSON.stringify(source) + ';';
 }
