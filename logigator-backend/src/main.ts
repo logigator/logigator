ac77ea253d3f4543c42aa1c8ac18d57848607299
Leo Preissegger <leo@webundco.com>
2020-09-18 15:30:23 +0200

    add docker setup

--- logigator-backend/src/main.ts
+++ logigator-backend/src/main.ts
@@ -72,7 +72,7 @@ async function bootstrap() {
 		currentUserChecker: action => action.request.user
 	});
 
-	app.listen(80, () => {
+	app.listen(3000, () => {
 		console.log('App started successfully');
 	});
 }
