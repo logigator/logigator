5fe9dc8397207c3aee9919f22bd48eaa141cb124
Leo Preissegger <leo@webundco.com>
2020-09-20 17:15:34 +0200

    can open popups

--- logigator-backend/resources/private/js/global.js
+++ logigator-backend/resources/private/js/global.js
@@ -22,3 +22,28 @@ function siteHeaderPartial(node) {
 siteHeaderPartial(document.querySelector('.partial-site-header'));
 
 // ------------------------------------------------------------------------------------------------
+
+/**
+ * @param {HTMLElement} node
+ */
+function popupPartial(node) {
+	let opened = Bem.hasState(node, 'open');
+
+	const triggers = Bem.data(node, 'triggers').split('::');
+	triggers.forEach(trigger => {
+		document.querySelector(trigger).addEventListener('click', () => {
+			if (!opened) {
+				Bem.setState(node, 'open', true);
+				opened = true;
+			}
+		});
+	});
+
+	Bem.element(node, 'close').addEventListener('click', () => {
+		Bem.setState(node, 'open', false);
+		opened = false;
+	});
+}
+document.querySelectorAll('.partial-popup').forEach(popup => popupPartial(popup));
+
+// ------------------------------------------------------------------------------------------------
