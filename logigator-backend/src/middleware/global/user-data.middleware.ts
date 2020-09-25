2232536b9852c82857c3215d21606abd66e1758e
Leo Preissegger <leo@webundco.com>
2020-09-25 23:32:07 +0200

    add default user image

--- logigator-backend/src/middleware/global/user-data.middleware.ts
+++ logigator-backend/src/middleware/global/user-data.middleware.ts
@@ -11,7 +11,7 @@ export class UserDataMiddleware implements ExpressMiddlewareInterface {
 			response.locals.user = {
 				username: (request.user as User).username,
 				email: (request.user as User).email,
-				image: (request.user as User).image?.publicUrl
+				image: (request.user as User).image?.publicUrl ?? '/assets/default-user.svg'
 			};
 		}
 		response.locals.preferences = request.session.preferences;
