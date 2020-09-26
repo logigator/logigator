f4c506b5287aec1fc182243bc56278f53711ac3f
Leo Preissegger <leo@webundco.com>
2020-09-26 15:56:58 +0200

    add section mixin render home hero

--- logigator-backend/src/controller/frontend/home.controller.ts
+++ logigator-backend/src/controller/frontend/home.controller.ts
@@ -1,14 +1,19 @@
 import {Controller, Get, Render, UseBefore} from 'routing-controllers';
 import {setTitleMiddleware} from '../../middleware/action/set-title-middleware';
+import {ConfigService} from '../../services/config.service';
 
 @Controller('/')
 export class HomeController {
+
+	constructor(private configService: ConfigService) {}
 
 	@Get('/')
 	@Render('home')
 	@UseBefore(setTitleMiddleware('TITLE.HOME'))
 	public async index() {
-		return '';
+		return {
+			editorUrl: this.configService.getConfig('domains').editor
+		};
 	}
 
 }
