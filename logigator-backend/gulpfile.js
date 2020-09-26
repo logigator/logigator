0ac3a2f7cc56320a9276b99b9bde1097306b35c6
Leo Preissegger <leo@webundco.com>
2020-09-26 16:08:17 +0200

    add css minifier

--- logigator-backend/gulpfile.js
+++ logigator-backend/gulpfile.js
@@ -1,22 +1,27 @@
+/* eslint-disable */
+
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const path = require('path');
 const babel = require('gulp-babel');
 const terser = require('gulp-terser');
 const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
 const sourceMaps = require('gulp-sourcemaps');
 
 
 gulp.task('scss:layouts', function() {
 	return gulp.src(path.join(__dirname, 'resources/private/scss/layouts/**/*.scss'))
-		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS({level: 2}))
 		.pipe(gulp.dest(path.join(__dirname, 'resources/public/css/layouts')));
 
 });
 
 gulp.task('scss:views', function() {
 	return gulp.src(path.join(__dirname, 'resources/private/scss/views/**/*.scss'))
-		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS({level: 2}))
 		.pipe(gulp.dest(path.join(__dirname, 'resources/public/css/views')));
 });
 
