2f2ff487a6a9a1315c05b828a3de9fd1d1c8dc84
Leo Preissegger <leo@webundco.com>
2020-10-05 00:03:11 +0200

    add helper methods to redis.service

--- logigator-backend/src/services/redis.service.ts
+++ logigator-backend/src/services/redis.service.ts
@@ -14,7 +14,80 @@ export class RedisService {
 			return this._redisClient;
 
 		this._redisClient = createClient(this.configService.getConfig('redis'));
-		return this.redisClient;
+		return this._redisClient;
+	}
+
+	public has(key: string): Promise<boolean> {
+		return new Promise<boolean>((resolve, reject) => {
+			this.redisClient.exists(key, (err, reply) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				resolve(!!reply);
+			});
+		});
+	}
+
+	public ttl(key: string): Promise<number> {
+		return new Promise<number>((resolve, reject) => {
+			this.redisClient.ttl(key, (err, reply) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				resolve(reply);
+			});
+		});
+	}
+
+	public setString(key: string, value: string, expire: number): Promise<void> {
+		return new Promise<void>((resolve, reject) => {
+			this.redisClient.setex(key, expire, value, (err) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				resolve();
+			});
+		});
+	}
+
+	public getString(key: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			this.redisClient.get(key, (err, reply) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				resolve(reply);
+			});
+		});
+	}
+
+	public setObject(key: string, value: any, expire: number): Promise<void> {
+		return new Promise<void>((resolve, reject) => {
+			this.redisClient.hmset(key, value, (err) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				this.redisClient.expire(key, expire);
+				resolve();
+			});
+		});
+	}
+
+	public getObject(key: string): Promise<any> {
+		return new Promise<any>((resolve, reject) => {
+			this.redisClient.hgetall(key, (err, reply) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				resolve(reply);
+			});
+		});
 	}
 
 }
