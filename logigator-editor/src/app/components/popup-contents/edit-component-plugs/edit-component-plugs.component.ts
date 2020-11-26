e62c454121d34d6fbaf4cf6c860e4571b4f35f2f
Leo Preissegger <preissegger.leo@gmail.com>
2020-11-26 13:21:20 +0100

    [editor] #15 render component plugs in edit-component-plugs.component

--- logigator-editor/src/app/components/popup-contents/edit-component-plugs/edit-component-plugs.component.ts
+++ logigator-editor/src/app/components/popup-contents/edit-component-plugs/edit-component-plugs.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {PopupContentComp} from '../../popup/popup-content-comp';
 import {Project} from '../../../models/project';
+import {AbstractControl, FormArray, FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
 	selector: 'app-edit-component-plugs',
@@ -9,11 +10,36 @@ import {Project} from '../../../models/project';
 })
 export class EditComponentPlugsComponent extends PopupContentComp<Project, never> implements OnInit {
 
-	constructor() {
+	public inputLabelsForm: FormGroup;
+	public outputLabelsForm: FormGroup;
+
+	constructor(private formBuilder: FormBuilder) {
 		super();
 	}
 
 	ngOnInit(): void {
+		const plugs = this.inputFromOpener.currState.allPlugs();
+		const inputPlugs = plugs.slice(0, this.inputFromOpener.numInputs);
+		const outputPlugs = plugs.slice(this.inputFromOpener.numInputs);
+
+		this.inputLabelsForm = this.formBuilder.group({
+			labels: this.formBuilder.array(inputPlugs.map(p => this.formBuilder.control(p.data ?? 'IN')))
+		});
+		this.outputLabelsForm = this.formBuilder.group({
+			labels: this.formBuilder.array(outputPlugs.map(p => this.formBuilder.control(p.data ?? 'OUT')))
+		});
+	}
+
+	public get inputLabelsControls(): AbstractControl[] {
+		return (this.inputLabelsForm.controls.labels as FormArray).controls;
+	}
+
+	public get outputLabelsControls(): AbstractControl[] {
+		return (this.outputLabelsForm.controls.labels as FormArray).controls;
+	}
+
+	public save() {
+
 	}
 
 }
