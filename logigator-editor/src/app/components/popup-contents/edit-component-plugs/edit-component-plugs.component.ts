ac851a346ba11c38cc5045dae6ad2735fc55c8fc
Andreas Schloessl <andreas.sch4@gmail.com>
2020-12-10 14:37:10 +0100

    [editor] #15 component in- and outputs can be dragged and saved

--- logigator-editor/src/app/components/popup-contents/edit-component-plugs/edit-component-plugs.component.ts
+++ logigator-editor/src/app/components/popup-contents/edit-component-plugs/edit-component-plugs.component.ts
@@ -1,22 +1,52 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {PopupContentComp} from '../../popup/popup-content-comp';
 import {Project} from '../../../models/project';
 import {AbstractControl, FormArray, FormBuilder, FormGroup} from '@angular/forms';
 import {ProjectsService} from '../../../services/projects/projects.service';
 import {Element} from '../../../models/element';
+import {fromEvent, Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
 	selector: 'app-edit-component-plugs',
 	templateUrl: './edit-component-plugs.component.html',
 	styleUrls: ['./edit-component-plugs.component.scss']
 })
-export class EditComponentPlugsComponent extends PopupContentComp<Project, never> implements OnInit {
+export class EditComponentPlugsComponent extends PopupContentComp<Project, never> implements OnInit, OnDestroy, AfterViewInit {
 
 	public inputLabelsForm: FormGroup;
 	public outputLabelsForm: FormGroup;
 
 	private _inputPlugs: Element[];
 	private _outputPlugs: Element[];
+
+	private _currentlyDragging: 'input' | 'output';
+	private _dragStart: number;
+	private _dragElement: {
+		element: HTMLElement,
+		pos: number,
+		adjustment: number
+	};
+
+	protected _destroySubject = new Subject<any>();
+
+	private _inputStates: {
+		element: HTMLElement,
+		pos: number,
+		adjustment: number
+	}[] = [];
+
+	private _outputStates: {
+		element: HTMLElement,
+		pos: number,
+		adjustment: number
+	}[] = [];
+
+	@ViewChild('compInputs', {static: true})
+	private _compInputs: ElementRef<HTMLFormElement>;
+
+	@ViewChild('compOutputs', {static: true})
+	private _compOutputs: ElementRef<HTMLFormElement>;
 
 	constructor(private formBuilder: FormBuilder, private projectsService: ProjectsService) {
 		super();
@@ -35,6 +65,34 @@ export class EditComponentPlugsComponent extends PopupContentComp<Project, never
 		});
 	}
 
+	ngAfterViewInit() {
+		for (const child of this._compInputs.nativeElement.children as unknown as HTMLElement[]) {
+			this._inputStates.push({
+				element: child,
+				pos: child.offsetTop,
+				adjustment: 0
+			});
+		}
+
+		for (const child of this._compOutputs.nativeElement.children as unknown as HTMLElement[]) {
+			this._outputStates.push({
+				element: child,
+				pos: child.offsetTop,
+				adjustment: 0
+			});
+		}
+
+		fromEvent(window, 'mouseup').pipe(
+			takeUntil(this._destroySubject)
+		).subscribe(() => this.mouseUp());
+
+		fromEvent(window, 'mousemove').pipe(
+			takeUntil(this._destroySubject)
+		).subscribe((e: MouseEvent) => this.mouseMove(e));
+
+		console.log(this.inputLabelsForm);
+	}
+
 	public get inputLabelsControls(): AbstractControl[] {
 		return (this.inputLabelsForm.controls.labels as FormArray).controls;
 	}
@@ -43,22 +101,76 @@ export class EditComponentPlugsComponent extends PopupContentComp<Project, never
 		return (this.outputLabelsForm.controls.labels as FormArray).controls;
 	}
 
-	public save() {
-		const plugIds = [
-			...shuffle(this._inputPlugs.map(p => p.id)),
-			...shuffle(this._outputPlugs.map(p => p.id))
-		];
-		console.log(this.inputFromOpener.setPlugConfiguration(plugIds, plugIds.map((p, i) => i + '')));
-		this.projectsService.labelsCustomComponentChanged(this.inputFromOpener);
+	mouseDown(event: MouseEvent, control: HTMLDivElement, column: 'input' | 'output') {
+		if (event.button !== 0)
+			return;
+		event.preventDefault();
+		this._currentlyDragging = column;
+		this._dragStart = event.clientY;
 
-		// only fo testing
-		function shuffle(a) {
-			for (let i = a.length - 1; i > 0; i--) {
-				const j = Math.floor(Math.random() * (i + 1));
-				[a[i], a[j]] = [a[j], a[i]];
+		this._dragElement = this._inputStates.find(x => x.element === control) || this._outputStates.find(x => x.element === control);
+		this._dragElement.element.style.transitionDuration = '0s';
+	}
+
+	mouseUp() {
+		if (this._currentlyDragging) {
+			this._dragElement.element.style.transitionDuration = '';
+			for (const child of this._currentlyDragging === 'input' ? this._inputStates : this._outputStates) {
+				child.adjustment = Math.round(Number(child.element.style.top.replace('px', '') || '0') / child.element.offsetHeight);
 			}
-			return a;
+			this._dragElement.element.style.top = (this._dragElement.adjustment * this._dragElement.element.offsetHeight) + 'px';
+			this._currentlyDragging = undefined;
 		}
+	}
+
+	mouseMove(e: MouseEvent) {
+		if (this._currentlyDragging) {
+			const adjustment = this._dragElement.adjustment * this._dragElement.element.offsetHeight;
+			const offset = e.clientY - this._dragStart + adjustment;
+
+			if (offset + this._dragElement.pos + this._dragElement.element.offsetHeight > this._compInputs.nativeElement.offsetHeight)
+				return;
+			else if (offset + this._dragElement.pos < 0)
+				return;
+
+			this._dragElement.element.style.top = offset + 'px';
+
+			for (const child of this._currentlyDragging === 'input' ? this._inputStates : this._outputStates) {
+				if (child === this._dragElement)
+					continue;
+
+				const pos = child.pos + child.adjustment * child.element.offsetHeight;
+
+				if (pos + child.element.offsetHeight / 2 <= this._dragElement.pos + adjustment
+					&& pos + child.element.offsetHeight / 2 > this._dragElement.element.offsetTop) {
+					child.element.style.top = this._dragElement.element.offsetHeight + child.adjustment * child.element.offsetHeight + 'px';
+				} else if (pos - child.element.offsetHeight / 2 >= this._dragElement.pos + adjustment
+					&& pos - child.element.offsetHeight / 2 < this._dragElement.element.offsetTop) {
+					child.element.style.top = -this._dragElement.element.offsetHeight + child.adjustment * child.element.offsetHeight + 'px';
+				} else {
+					child.element.style.top = child.adjustment * child.element.offsetHeight + 'px';
+				}
+			}
+		}
+	}
+
+	public save() {
+		const sorted = [
+			...this._inputStates
+				.map((x, i) => [x, this._inputPlugs[i], this.inputLabelsForm.value.labels[i]])
+				.sort((a, b) => a[0].element.offsetTop - b[0].element.offsetTop),
+			...this._outputStates
+				.map((x, i) => [x, this._outputPlugs[i], this.outputLabelsForm.value.labels[i]])
+				.sort((a, b) => a[0].element.offsetTop - b[0].element.offsetTop)
+		];
+		this.inputFromOpener.setPlugConfiguration(sorted.map(x => x[1].id), sorted.map(x => x[2]));
+		this.projectsService.labelsCustomComponentChanged(this.inputFromOpener);
+		this.requestClose.emit();
+	}
+
+	ngOnDestroy(): void {
+		this._destroySubject.next();
+		this._destroySubject.complete();
 	}
 
 }
