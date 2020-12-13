6acb62acd260fe2797c372be91f5b54f3fd64dc7
Andreas <andreas.sch4@gmail.com>
2020-12-13 20:13:43 +0100

    [editor] fixed svg connection point scaling

--- logigator-editor/src/app/services/image-export/svg-image-exporter.ts
+++ logigator-editor/src/app/services/image-export/svg-image-exporter.ts
@@ -175,7 +175,7 @@ export class SvgImageExporter {
 		point.setAttribute('x', (pos.x + 0.5) * this.gridSize + this.offset.x - 2.5 + '');
 		point.setAttribute('y', (pos.y + 0.5) * this.gridSize + this.offset.y - 2.5 + '');
 		point.setAttribute('width', this.gridSize < 8 ? '3' : '5');
-		point.setAttribute('height', '5');
+		point.setAttribute('height', this.gridSize < 8 ? '3' : '5');
 		point.setAttribute('class', 'cp');
 		this._svg.appendChild(point);
 	}
