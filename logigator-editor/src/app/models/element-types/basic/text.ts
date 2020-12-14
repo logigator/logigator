77da7f8cc9e7c7910a887b726ad380b0687ae164
Andreas <andreas.sch4@gmail.com>
2020-12-14 12:17:23 +0100

    Revert "[editor] text element is editable"
    This reverts commit 49e82e2d

--- logigator-editor/src/app/models/element-types/basic/text.ts
+++ logigator-editor/src/app/models/element-types/basic/text.ts
@@ -49,7 +49,7 @@ export const text: ElementType = {
 		if (nText === oText || nText === undefined) return;
 		projectsSer.currProject.setData(id, nText);
 	},
-	canEditType: true
+	canEditType: false
 };
 
 export type TextData = string;
