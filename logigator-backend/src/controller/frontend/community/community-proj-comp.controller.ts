01516a2c4ffb941b028e4225ffd77e7b1d5e2a63
Leo Preissegger <preissegger.leo@gmail.com>
2020-12-20 13:51:19 +0100

    [editor] #35 add community-proj-comp page

--- logigator-backend/src/controller/frontend/community/community-proj-comp.controller.ts
+++ logigator-backend/src/controller/frontend/community/community-proj-comp.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Param, Render} from 'routing-controllers';
+import {Controller, Get, NotFoundError, Param, Render} from 'routing-controllers';
 import {ConfigService} from '../../../services/config.service';
 import {TranslationService} from '../../../services/translation.service';
 import {InjectRepository} from 'typeorm-typedi-extensions';
@@ -20,13 +20,55 @@ export class CommunityProjCompController {
 	@Get('/project/:link')
 	@Render('community-proj-comp')
 	public async project(@Param('link') link: string, @Preferences() preferences: UserPreferences) {
-		return {};
+		const project = (await this.projectRepo.findOne({
+			where: {
+				link
+			}
+		}));
+		if (!project)
+			throw new NotFoundError();
+
+		const user = await project.user;
+
+		return {
+			...project,
+			previewDark: project.previewDark?.publicUrl ?? '/assets/default-preview.svg',
+			previewLight: project.previewLight?.publicUrl ?? '/assets/default-preview.svg',
+			editorUrl: this.configService.getConfig('domains').editor + '/share/' + project.link,
+			cloneUrl: '/community/clone/' + project.link,
+			stargazers: '/community/project/' + project.link + '/stargazers',
+			username: user.username,
+			userImage: user.image?.publicUrl ?? '/assets/default-user.svg',
+			userUrl: 'community/user/' + user.id,
+			stars: 10
+		};
 	}
 
 	@Get('/component/:link')
 	@Render('community-proj-comp')
 	public async component(@Param('link') link: string, @Preferences() preferences: UserPreferences) {
-		return {};
+		const comp = (await this.componentRepo.findOne({
+			where: {
+				link
+			}
+		}));
+		if (!comp)
+			throw new NotFoundError();
+
+		const user = await comp.user;
+
+		return {
+			...comp,
+			previewDark: comp.previewDark?.publicUrl ?? '/assets/default-preview.svg',
+			previewLight: comp.previewLight?.publicUrl ?? '/assets/default-preview.svg',
+			editorUrl: this.configService.getConfig('domains').editor + '/share/' + comp.link,
+			cloneUrl: '/community/clone/' + comp.link,
+			stargazers: '/community/component/' + comp.link + '/stargazers',
+			username: user.username,
+			userImage: user.image?.publicUrl ?? '/assets/default-user.svg',
+			userUrl: 'community/user/' + user.id,
+			stars: 10
+		};
 	}
 
 }
