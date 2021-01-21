8e1c66fe7ee315044ff186f2274caca1ec38ac1e
Leo Preissegger <preissegger.leo@gmail.com>
2021-01-21 13:23:46 +0100

    [backend] append reported errors to configured file

--- logigator-backend/src/controller/api/report-error.controller.ts
+++ logigator-backend/src/controller/api/report-error.controller.ts
@@ -1,14 +1,26 @@
 import {Body, JsonController, Post, UseInterceptor} from 'routing-controllers';
 import {ApiInterceptor} from '../../interceptors/api.interceptor';
 import {ReportError} from '../../models/request/api/report-error/report-error';
+import {ConfigService} from '../../services/config.service';
+import * as path from 'path';
+import {promises as fs} from 'fs';
 
 @JsonController('/api/report-error')
 @UseInterceptor(ApiInterceptor)
 export class ReportErrorController {
 
+	constructor(private configService: ConfigService) {}
+
 	@Post('/')
-	public reportError(@Body() body: ReportError) {
-		console.log(body);
+	public async reportError(@Body() body: ReportError) {
+		const errorLogLocation = path.join(this.configService.projectRootPath, this.configService.getConfig<any>('environment').reportErrorLog);
+		const toAppend = new Date().toUTCString() +
+			`\n${body.message}` +
+			`\n${body.stack}` +
+			`\n${body.userMessage}` +
+			'\n-----------------------------------------------------------------------------------\n';
+
+		await fs.appendFile(errorLogLocation, toAppend);
 
 		return { success: true };
 	}
