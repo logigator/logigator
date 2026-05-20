194c5b0e83212b8f949aecaf2d55d4e6e75bbbb1
andreas <andreas.sch4@gmail.com>
2026-05-20 21:28:17 +0200

    [editor] added id-allocator

---
+++ logigator-editor-v2/src/app/utils/id-allocator.ts
@@ -0,0 +1,13 @@
+export class IdAllocator {
+	private _counter = 0;
+
+	next(): number {
+		return this._counter++;
+	}
+
+	bump(id: number): void {
+		if (id >= this._counter) {
+			this._counter = id + 1;
+		}
+	}
+}
