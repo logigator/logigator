fc3d5f352453df98b301937a6ff4549852f96277
Andreas <andreas.sch4@gmail.com>
2026-06-30 01:58:48 +0000

    [editor] remove PrimeNG (providers, theme preset, dependencies)

--- logigator-editor-v2/src/app/app.config.ts
+++ logigator-editor-v2/src/app/app.config.ts
@@ -5,27 +5,12 @@ import {
 } from '@angular/core';
 import { provideTransloco } from '@jsverse/transloco';
 import { TranslationLoaderService } from './translation/translation-loader.service';
-import { providePrimeNG } from 'primeng/config';
-import { ConfirmationService, MessageService } from 'primeng/api';
-import { DialogService } from 'primeng/dynamicdialog';
-import { AppTheme } from './app.theme';
 import { provideHttpClient } from '@angular/common/http';
 import { provideTranslocoPersistLang } from '@jsverse/transloco-persist-lang';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
-    providePrimeNG({
-      theme: {
-        preset: AppTheme,
-        options: {
-          darkModeSelector: '.dark-mode'
-        }
-      }
-    }),
-    ConfirmationService,
-    MessageService,
-    DialogService,
     provideTransloco({
       config: {
         defaultLang: 'en',
